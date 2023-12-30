f711ddf0f046984d9f03af89d4dd30d7e1f69a19
Ayokunle Bamisile-Aje <admit_kunle@yahoo.com>
2023-12-30 02:12:59 -0500

    Fix catalog.js to reference images correctly

--- data/catalog.js
+++ data/catalog.js
@@ -1,43 +1,43 @@
 let catalog = [
     {
         id: 0,
-        image: '../images/catalog/borsao.avif',
+        image: './images/catalog/borsao.avif',
         name: 'Borsao',
         price: 7500000
     },
     {
         id: 1,
-        image: '../images/catalog/broadleaf.avif',
+        image: './images/catalog/broadleaf.avif',
         name: 'broadleaf',
         price: 7500000
     },
     {
         id: 2,
-        image: '../images/catalog/chianti.avif',
+        image: './images/catalog/chianti.avif',
         name: 'chianti',
         price: 7500000
     },
     {
         id: 3,
-        image: '../images/catalog/paros.avif',
+        image: './images/catalog/paros.avif',
         name: 'paros',
         price: 7500000
     },
     {
         id: 4,
-        image: '../images/catalog/rasteau.avif',
+        image: './images/catalog/rasteau.avif',
         name: 'rasteau',
         price: 7500000
     },
     {
         id: 5,
-        image: '../images/catalog/rose.avif',
+        image: './images/catalog/rose.avif',
         name: 'rose',
         price: 7500000
     },
     {
         id: 6,
-        image: '../images/catalog/talamonti.avif',
+        image: './images/catalog/talamonti.avif',
         name: 'talamonti',
         price: 7500000
     }
